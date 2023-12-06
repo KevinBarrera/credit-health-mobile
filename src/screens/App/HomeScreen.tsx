102867ab339fd2eab79434337f81c58ccbbb920f
KevinBarrera <kev97barrera@gmail.com>
2023-12-06 16:49:49 -0600

    Aux-temporal HomeScreen added

---
+++ src/screens/App/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text>HomeScreen</Text>
+  </View>
+);
+
+export { HomeScreen };
+
+const styles = StyleSheet.create({
+  container: { flex: 1 }
+});
